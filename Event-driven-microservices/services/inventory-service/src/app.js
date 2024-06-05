60ff0ca7a17657e536bfbdf5936f548afa828b50
Mahatma22 <mahatma.afif@gmail.com>
2024-06-05 12:26:26 +0700

    front end template

---
+++ Event-driven-microservices/services/inventory-service/src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Import routes
+const homeRoutes = require('./routes/homeRoutes');
+
+// Use routes
+app.use('/', homeRoutes);
+
+// Serve index.ejs from views directory
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3099;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${3099}`));
+
+
+
